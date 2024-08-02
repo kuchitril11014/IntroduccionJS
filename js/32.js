262b167b31549c824e697f83fdf57d67537e65e8
kuchitril11014 <rafaelsoto11014@outlook.com>
2024-08-01 17:42:36 -0700

    finalizando introduccion a js

--- js/32.js
+++ js/32.js
@@ -6,10 +6,28 @@ formulario.addEventListener('submit', (e)=>{
 
     const nombre = document.querySelector('#nombre').value
     const password = document.querySelector('#password').value
+
+    //prevenir nuevas alertas
+    const alertaPrevia = document.querySelector('.alerta')
+    alertaPrevia?.remove()
+
+    const alerta = document.createElement('DIV')
+    alerta.classList.add('alerta','text-white','uppercase','text-sm','text-center','p-2', 'font-black')
+    // alerta.textContent = 'Contenido alerta'
+    // console.log(alerta);
+    // alerta.classList.add('clase-1','clase-2','clase-3')
     
-    if (nombre === ' ' || password === ' ') {
-        console.log('Todos los campos son obligatorios')
+    
+    if(nombre === '' || password === '') {
+        alerta.textContent = 'Todos los campos son obligatorios...'
+        alerta.classList.add('bg-red-500')
     } else {
-            console.log('Todo bien iniciando sesion...')
+        alerta.textContent = 'Todo bien ...' 
+        alerta.classList.add('bg-green-500')
+    
     }
+  formulario.appendChild(alerta)
+  setTimeout( ()=>{
+    alerta.remove()
+  }, 3000 )
 })
